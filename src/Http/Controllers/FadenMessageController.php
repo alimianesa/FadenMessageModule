fb4ed6abac3bdc2a0ca69df9af412a9d67e13523
alimianesa <57527510+alimianesa@users.noreply.github.com>
2020-05-05 16:19:06 +0430

    Update FadenMessageController.php

--- src/Http/Controllers/FadenMessageController.php
+++ src/Http/Controllers/FadenMessageController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Faden\FadenMessageModule\Controllers;
+namespace Faden\FadenMessageModule\Http\Controllers;
 
 use Alive2212\LaravelSmartRestful\SmartCrudController;
-use App\FadenMessage;
+use Faden\FadenMessageModule\FadenMessage;
 
+use App\Http\Controllers\Controller;
 use Faden\FadenMessageModule\Notifications\MailNotification;
 use App\User;
 use Illuminate\Http\Request;
@@ -13,20 +14,24 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Notification;
 
 
-class FadenMessageController extends SmartCrudController
+class FadenMessageController extends Controller
 {
-    public function initController()
+
+    public function send(Request $request)
     {
-        $this->model  = new FadenMessage();
+
+        $message = FadenMessage::create($request);
+
+        Notification::route('mail', $message->to)->notify( new MailNotification($message));
     }
 
-    public function send($message)
+    public function test()
     {
         $data = [
-            'message_type' => 'email',
+            'message_type' => 1,
             'body' => "User Created",
             'title' =>'title',
-            'to' =>'ali.salimiansas2@gmial.com',
+            'to' =>'ali.salimiansas2@gmail.com',
             'author_id'  => 1,
             'priority' =>  "immediately",
             'sent_at' => time() ,
@@ -35,4 +40,21 @@ class FadenMessageController extends SmartCrudController
 
         Notification::route('mail', $message->to)->notify( new MailNotification($message));
     }
+
+    public function push(Request $request)
+    {
+        $message=$request;
+
+        Http::withHeaders([
+            'Authorization' => 'key=AAAApw-yf2U:APA91bE_baOttqXzE7q39jozuBIwSBChFNwKtx0U7TuloyAQJiuvJO1K7gmBPpK8qjKp12mBufpK8I2ndQug8Emzq-BJUcHYSNCKK3To4Uy0D2B6IMbOnJizhDGEw5tnS6yb4sNLDOjP',
+            'Content-Type' => 'application/json'
+        ])->post('https://fcm.googleapis.com/fcm/send', [
+            "to" => $message->to,
+            "notification" => [
+                'body' => $message->push_body,
+                'title' => $message->push_title
+            ]
+        ]);
+    }
+
 }
